5d26a31839d13dba4e70c56b4087969499a0067a
renuetake <ren.of.bamboo616@gmail.com>
2024-02-25 16:11:09 +0900

    Chapter8

---
+++ Java/JavaBronzeSampleCode/server/src/Chapter8/Sample8_3.java
@@ -0,0 +1,23 @@
+package Chapter8;
+interface MyInterface {
+    // int a; // 初期化していないのでコンパイルエラー
+    int b               = 10;
+    final int c         = 20;
+    public int d        = 30;
+    static int e        = 40;
+    public final int f  = 50;
+
+    public abstract void methodA();
+    abstract void methodB();
+    void methodC();
+    // protected abstract void methodD(); // protectedは使用不可。コンパイルエラー
+}
+
+class MyClass implements MyInterface {
+    public void methodA() {}
+    public void methodB() {}
+    public void methodC() {}
+}
+public class Sample8_3 {
+    
+}
